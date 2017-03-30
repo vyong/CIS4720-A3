52930e5ce81e12fadcf60a0dcf5760a8130e3723
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-03-30 16:20:50 -0400

    Finished SURF image-compositor

--- surf.py
+++ surf.py
@@ -7,8 +7,10 @@ import sys
 targetDir = sys.argv[1]
 
 imgR = cv2.imread(os.path.join(targetDir, 'r.tif'), 0)
-imgG = cv2.imread(os.path.join(targetDir, 'b.tif'), 0)
-imgB = cv2.imread(os.path.join(targetDir, 'g.tif'), 0)
+imgG = cv2.imread(os.path.join(targetDir, 'g.tif'), 0)
+imgB = cv2.imread(os.path.join(targetDir, 'b.tif'), 0)
+height, width = imgR.shape[:2] # Guaranteed to be same size for easy compositing
+shape = (width, height)
 
 surf = cv2.xfeatures2d.SURF_create()
 
@@ -18,22 +20,24 @@ kpB, desB = surf.detectAndCompute(imgB, None)
 
 bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
 
-#clusters = np.array([desR])
-#bf.add(clusters)
-#bf.train()
-
-#matches = bf.match(desG)
-
-
 matches = bf.match(desR, desG)
+goodMatches = [match for match in matches if match.distance < 300]
 
-matches = sorted(matches, key = lambda x:x.distance)
-print len(matches)
+matchR = np.array([kpR[match.queryIdx].pt for match in goodMatches])
+matchG = np.array([kpG[match.trainIdx].pt for match in goodMatches])
+rgH, mask = cv2.findHomography(matchG, matchR, cv2.RANSAC)
 
-imgOut = np.array([])
-imgOut = cv2.drawMatches(imgR, kpR, imgG, kpG, matches[:100], imgOut, flags=2)
+matches = bf.match(desR, desB)
+goodMatches = [match for match in matches if match.distance < 300]
+matchR = np.array([kpR[match.queryIdx].pt for match in goodMatches])
+matchB = np.array([kpB[match.trainIdx].pt for match in goodMatches])
+rbH, mask = cv2.findHomography(matchB, matchR, cv2.RANSAC)
 
-cv2.namedWindow("Display", cv2.WINDOW_NORMAL)
-cv2.imshow("Display", imgOut)
-cv2.waitKey(0);
-#print cv2.__version__
+warpG = cv2.warpPerspective(imgG, rgH, shape)
+warpB = cv2.warpPerspective(imgB, rbH, shape)
+
+stacked = np.dstack((imgR, warpG, warpB))
+
+cv2.namedWindow("warped", cv2.WINDOW_NORMAL)
+cv2.imshow("warped", stacked)
+cv2.waitKey(0)
